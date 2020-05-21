088ba4f34e3cb7c57a919a4670c4e72b30f17723
金山裕紀 <yukikanayama@kanayamayuukinoMacBook-Air.local>
2020-05-21 19:02:15 +0900

    Ruby on Railsの学習

---
+++ app/helpers/todoliste_helper.rb
@@ -0,0 +1,2 @@
+module TodolisteHelper
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '794e74cf00198a008a489c3c9d878dbf7cfa3375a46153ee43fe07bc21cab606fed1527340e45719194c12952b8fdc5f5c119cc249689b5b7c07c938870bb208'
